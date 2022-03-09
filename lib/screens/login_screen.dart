2f0da3b86d1e2143be992bb34799df88e4abed1b
Edgar Lopez <16171409@itculiacan.edu.mx>
2022-03-09 16:31:36 -0700

    Hecho el inicio de sesion favor de repetirlo sin ver

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iniciosecion_prueba/providers/login_form_provider.dart';
+import 'package:iniciosecion_prueba/ui/input_decorations.dart';
 import 'package:iniciosecion_prueba/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
    
@@ -22,12 +25,119 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox( height: 10, ),
                     Text('Login', style: Theme.of(context).textTheme.headline4,),
                     const SizedBox( height: 30, ),
+                    
+                    ChangeNotifierProvider(
+                      create: ( _ ) => LoginFormProvider(),
+                      child: const _LoginForm(),
+                    ),
+                    
+
                   ]
                 ),
-              )
+              ),
+
+              const SizedBox( height: 50,),
+              const Text('IE Soluciones', style: TextStyle( fontSize: 20, fontWeight: FontWeight.bold ))
+
             ],
           ),
         )
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    final loginForm = Provider.of<LoginFormProvider>(context);
+
+    return Container(
+      child: Form(
+        key: loginForm.formKey,
+        autovalidateMode: AutovalidateMode.onUserInteraction,
+        child: Column(
+          children: [
+
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: 'edgar12@gmail.com',
+                labelText: 'Correo Electronico',
+                prefixIcon: Icons.alternate_email_rounded 
+              ),
+              onChanged: ( value ) => loginForm.email = value,
+              validator: ( value ) {
+                
+                String pattern = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                RegExp regExp  = RegExp(pattern);
+
+                return regExp.hasMatch(value ?? '')
+                  ? null
+                  : 'El dato ingresado no es un Correo';
+
+              },
+
+            ),
+
+            const SizedBox( height: 30,),
+
+            TextFormField(
+              autocorrect: false,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              decoration: InputDecorations.authInputDecoration(
+                hintText: '*****',
+                labelText: 'Contraseña',
+                prefixIcon: Icons.lock_outline
+              ),
+              onChanged: ( value ) => loginForm.password = value,
+              validator: ( value ) {
+                
+                return ( value != null && value.length >= 6 ) 
+                  ? null
+                  : 'La contraseña debe tener 6 caracteres';
+                  
+              },
+            ),
+
+            const SizedBox( height: 30,),
+
+            MaterialButton(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              disabledColor: Colors.grey,
+              elevation: 0,
+              color: Colors.deepPurple,
+              child: Container(
+                padding: const EdgeInsets.symmetric( horizontal: 80, vertical: 15),
+                child: Text(
+                  loginForm.isLoading
+                  ? 'Espere'
+                  : 'Ingresar',
+                  style: const TextStyle ( color: Colors.white),
+                ),
+              ),
+              onPressed: loginForm.isLoading ? null : () async {
+                FocusScope.of(context).unfocus();
+
+                if( !loginForm.isValidForm() ) return;
+
+                loginForm.isLoading = true;
+
+                await Future.delayed( const Duration(seconds: 2));
+
+                loginForm.isLoading = false;
+
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+            )
+
+          ]
+        ),
       ),
     );
   }
